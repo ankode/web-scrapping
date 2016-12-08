497f8f0b6f4c67f96fc4daed8ea6b64bde52b4ff
Ankush Anshuman <ankush3107@gmail.com>
2016-12-08 23:21:00 +0530

    deleting old files

--- ion-logging/ion_login_chrome.py
+++
@@ -1,9 +0,0 @@
-# code written by ANKUSH ANSHUMAN
-# github.com/ankode
-
-from selenium.webdriver import Chrome
-b = Chrome('C:\\chromedriver.exe')
-b.get('http://172.16.16.16/logout')
-b.find_element_by_name("username").send_keys("")
-b.find_element_by_name("password").send_keys("")
-b.find_element_by_name("login").click()
