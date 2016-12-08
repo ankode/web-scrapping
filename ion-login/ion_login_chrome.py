ba423c2a3b350551fc232cbea319934246bcfa55
Ankush Anshuman <ankush3107@gmail.com>
2016-12-08 23:18:26 +0530

    added seperate variables for username and password

--- ion-login/ion_login_chrome.py
+++ ion-login/ion_login_chrome.py
@@ -1,9 +1,12 @@
 # code written by ANKUSH ANSHUMAN
 # github.com/ankode
 
+username=""
+password=""
+
 from selenium.webdriver import Chrome
 b = Chrome('C:\\chromedriver.exe')
 b.get('http://172.16.16.16/logout')
-b.find_element_by_name("username").send_keys("")
-b.find_element_by_name("password").send_keys("")
+b.find_element_by_name("username").send_keys(username)
+b.find_element_by_name("password").send_keys(password)
 b.find_element_by_name("login").click()
